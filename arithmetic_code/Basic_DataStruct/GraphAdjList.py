51e9190170cf3c1bc13280ce290b0c33c92f8469
lzh <15612191579@163.com>
2023-11-08 21:59:20 +0800

    邻接表的BFS和DFS

--- arithmetic_code/Basic_DataStruct/GraphAdjList.py
+++ arithmetic_code/Basic_DataStruct/GraphAdjList.py
@@ -46,6 +46,36 @@ class GraphAdjList:
         for k, v in self.edges.items():
             print(k, v)
 
+    def BFS(self):
+        cur = self.vertex
+        queue = [cur[0]]
+        visited = set()
+        visited.add(cur[0])
+        res = []
+        while queue:
+            node = queue.pop(0)
+            res.append(node)
+            lst_node = self.edges[node]
+            for i in lst_node:
+                if i not in visited:
+                    visited.add(i)
+                    queue.append(i)
+        return res
+
+    def dfs(self):
+        res = []
+        visited = set()
+        self.dfs_helper(visited, res, self.vertex[0])
+        return res
+
+    def dfs_helper(self, visited, res, vet):
+        res.append(vet)
+        visited.add(vet)
+        lst_node = self.edges[vet]
+        for i in lst_node:
+            if i not in visited:
+                self.dfs_helper(visited, res, i)
+
 
 x = [1, 2, 5, 4, 3]
 edges = [[1, 3], [1, 5], [3, 2], [2, 5], [2, 4], [5, 4]]
@@ -56,12 +86,14 @@ for i in x:
     graph.add_vertex(i)
 for edge in edges:
     graph.add_edges(edge[0], edge[1])
-graph.print()
-graph.add_vertex(6)
-graph.print()
-graph.add_edges(1, 6)
-graph.print()
-graph.delete_edge(2, 5)
-graph.print()
-graph.delete_vertex(1)
-graph.print()
+# graph.print()
+# graph.add_vertex(6)
+# graph.print()
+# graph.add_edges(1, 6)
+# graph.print()
+# graph.delete_edge(2, 5)
+# graph.print()
+# graph.delete_vertex(1)
+# graph.print()
+print(graph.BFS())
+print(graph.dfs())
